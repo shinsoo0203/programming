06489ea45f630bda4bcf424cce33871f56a95760
gravity101 <gravity101@konkuk.ac.kr>
2021-07-29 23:19:21 +0900

    Update 7/29

--- gravity101/7-29 괄호 변환.py
+++ gravity101/7-29 괄호 변환.py
@@ -1,4 +1,53 @@
-#1차 시도
+def solution(p):
+    answer = ''
+
+    if len(p) == 0:
+        return answer
+
+    u = ''
+    v = ''
+    left, right = 0, 0
+
+    for i in range(len(p)):
+        if p[i] == '(':
+            left += 1
+        else:
+            right += 1
+        if left == right:
+            u = p[:i + 1]
+            v = p[i + 1:]
+            break
+
+    print(u, v)
+    stack = []
+    for i in range(len(u)):
+        if not stack:
+            stack.append(u[0])
+        elif u[i] == '(':
+            stack.append(u[i])
+        elif stack[-1] == '(' and u[i] == ')':
+            stack.pop()
+
+    if not stack:
+        answer += u
+        answer += solution(v)
+
+    else:
+        answer += '('
+        answer += solution(v)
+        answer += ')'
+
+        u = u[1:-1]
+
+        for t in range(len(u)):
+            if u[t] == '(':
+                answer += ')'
+            else:
+                answer += '('
+    return answer
+
+
+#1차     시도
 
 # def solution(p):
 #     answer = '
@@ -46,54 +95,54 @@
 
 
 
-
-def solution(p):
-    p = list(p)
-    answer = ''
-
-    while len(p) > 0:
-        u = []
-        v = []
-        left, right = 0, 0
-
-        for i in range(len(p)):
-            if p[i] == '(':
-                left += 1
-            else:
-                right += 1
-            if left == right:
-                u = p[:i + 1]
-                v = p[i + 1:]
-                break
-        p = v
-        print(p)
-        stack = []
-        for i in range(len(u)):
-
-            if u[0] == '(' :
-                stack.append(u[0])
-            elif u[i] == '(':
-                stack.append(u[i])
-            elif stack[-1] == '(' and u[i] == ')':
-                stack.pop()
-
-        if len(stack) == 0:
-            for i in u:
-                answer += i
-        else:
-            u.pop(0)
-            u.pop(-1)
-
-            for t in range(len(u)):
-                if u[t] == '(':
-                    u[t] = ')'
-                else:
-                    u[t] = '('
-
-            answer += '('
-
-            for t in range(len(u)):
-                answer += u[t]
-            answer += ')'
-
-    return answer
+# 2차시
+# def solution(p):
+#     p = list(p)
+#     answer = ''
+#
+#     while len(p) > 0:
+#         u = []
+#         v = []
+#         left, right = 0, 0
+#
+#         for i in range(len(p)):
+#             if p[i] == '(':
+#                 left += 1
+#             else:
+#                 right += 1
+#             if left == right:
+#                 u = p[:i + 1]
+#                 v = p[i + 1:]
+#                 break
+#         p = v
+#         print(p)
+#         stack = []
+#         for i in range(len(u)):
+#
+#             if not stack:
+#                 stack.append(u[0])
+#             elif u[i] == '(':
+#                 stack.append(u[i])
+#             elif stack[-1] == '(' and u[i] == ')':
+#                 stack.pop()
+#
+#         if not stack:
+#             for i in u:
+#                 answer += i
+#         else:
+#             u.pop(0)
+#             u.pop(-1)
+#
+#             for t in range(len(u)):
+#                 if u[t] == '(':
+#                     u[t] = ')'
+#                 else:
+#                     u[t] = '('
+#
+#             answer += '('
+#
+#             for t in range(len(u)):
+#                 answer += u[t]
+#             answer += ')'
+#
+#     return answer
